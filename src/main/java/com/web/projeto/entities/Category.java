ee4c6be8093fa94a487c554b1b21544aed45b3df
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-10-05 20:16:36 -0300

    Product

--- src/main/java/com/web/projeto/entities/Category.java
+++ src/main/java/com/web/projeto/entities/Category.java
@@ -3,7 +3,9 @@ package com.web.projeto.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,8 +15,9 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -38,6 +41,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/web/projeto/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.web.projeto.repository;
+
+import com.web.projeto.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
