a1bcfc2cc1944343f47c7d5b83fc1aca26d93e97
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-10-05 18:30:31 -0300

    OrderStatus enum

--- src/main/java/com/web/projeto/config/TestConfig.java
+++ src/main/java/com/web/projeto/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.web.projeto.config;
 
 import com.web.projeto.entities.Order;
 import com.web.projeto.entities.User;
+import com.web.projeto.entities.enums.OrderStatus;
 import com.web.projeto.repository.OrderRepository;
 import com.web.projeto.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "91234567", "123456");
 
 
-        Order o1 = new Order(null, Instant.parse("2024-10-05T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-09-06T15:22:10Z"), u1);
-        Order o3 = new Order(null, Instant.parse("2024-08-22T08:31:14Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-10-05T19:53:07Z"), OrderStatus.PAID,u1);
+        Order o2 = new Order(null, Instant.parse("2024-09-06T15:22:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-08-22T08:31:14Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o2));
